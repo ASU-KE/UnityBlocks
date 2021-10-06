d0be66f005ca98f3f2c2dd0dbe1239b23cb12a7f
Nathan Rollins <nathan.rollins@asu.edu>
2021-10-06 13:57:46 -0700

    Commit dev config

---
+++ .dev/config/webpack.config.js
@@ -0,0 +1,123 @@
+/**
+ * WordPress dependencies
+ */
+const DependencyExtractionWebpackPlugin = require("@wordpress/dependency-extraction-webpack-plugin");
+
+/**
+ * Internal dependencies
+ */
+const postcssConfig = require("./postcss.config");
+const { hasBabelConfig } = require("./utils");
+
+/**
+ * External dependencies
+ */
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const RtlCssPlugin = require("rtlcss-webpack-plugin");
+const path = require("path");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const nodeSassGlobImporter = require("node-sass-glob-importer");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+module.exports = {
+	entry: {
+		unityblocks: path.resolve(process.cwd(), "src/blocks.js"),
+
+		// Styles
+		"unityblocks-editor": path.resolve(
+			process.cwd(),
+			"src/styles/editor.scss"
+		),
+		"unityblocks-style": path.resolve(
+			process.cwd(),
+			"src/styles/style.scss"
+		),
+	},
+
+	output: {
+		filename: "[name].js",
+		path: path.resolve(process.cwd(), "dist/"),
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.svg$/,
+				use: ["@svgr/webpack", "url-loader"],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					require.resolve("thread-loader"),
+					{
+						loader: require.resolve("babel-loader"),
+						options: {
+							// Babel uses a directory within local node_modules
+							// by default. Use the environment variable option
+							// to enable more persistent caching.
+							cacheDirectory:
+								process.env.BABEL_CACHE_DIRECTORY || true,
+
+							// Provide a fallback configuration if there's not
+							// one explicitly available in the project.
+							...(!hasBabelConfig() && {
+								babelrc: false,
+								configFile: false,
+								presets: [
+									require.resolve(
+										"@wordpress/babel-preset-default"
+									),
+								],
+							}),
+						},
+					},
+				],
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+						options: {
+							url: false,
+							sourceMap: !isProduction,
+						},
+					},
+					{
+						loader: "postcss-loader",
+						options: {
+							...postcssConfig,
+							sourceMap: !isProduction,
+						},
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: !isProduction,
+							sassOptions: {
+								importer: nodeSassGlobImporter(),
+							},
+						},
+					},
+				],
+			},
+		],
+	},
+
+	stats: {
+		children: false,
+		modules: false,
+		warnings: false,
+	},
+
+	plugins: [
+		new DependencyExtractionWebpackPlugin({ injectPolyfill: true }),
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+		}),
+		new FixStyleOnlyEntriesPlugin(),
+	],
+};
