055228b4093f2fda0bcc4459db432c10e148a949
Nathan Rollins <nathan.rollins@asu.edu>
2021-10-06 20:44:40 -0700

    Comit blocks.js entrypoint

---
+++ src/blocks.js
@@ -0,0 +1,61 @@
+/**
+ * WordPress dependencies
+ */
+import { registerBlockType } from '@wordpress/blocks';
+
+// Register block category
+import './utils/block-category';
+
+// Internal Extensions / Components
+import './components/gutter-control';
+import './components/form-label-colors';
+
+// Formats
+import './formats';
+
+// Categories Helper
+import { supportsCollections } from './utils/block-helpers';
+
+// Deprecated Blocks
+// import './js/deprecations/deprecate-unityblocks-buttons.js';
+// import './js/deprecations/deprecate-unityblocks-media-card.js';
+
+// Register Blocks
+import * as alert from './blocks/alert';
+
+/**
+ * Function to register an individual block.
+ *
+ * @param {Object} block The block to be registered.
+ */
+const registerBlock = ( block ) => {
+	if ( ! block ) {
+		return;
+	}
+
+	let { category } = block;
+
+	const { name, settings } = block;
+
+	if ( ! supportsCollections() && ! name.includes( 'gallery' ) ) {
+		category = 'unityblocks';
+	}
+
+	registerBlockType( name, {
+		category,
+		...settings,
+	} );
+};
+
+/**
+ * Function to register blocks provided by UnityBlocks.
+ */
+export const registerUnityBlocksBlocks = () => {
+	[
+		// accordion,
+		// accordionItem,
+		alert,
+	].forEach( registerBlock );
+};
+
+registerUnityBlocksBlocks();
