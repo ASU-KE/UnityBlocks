7ba2a3908e2728712d5ce48362c4e69dc6e54104
Nathan Rollins <nathan.rollins@asu.edu>
2022-01-14 09:02:42 -0700

    coding standards

--- includes/enqueue-blocks/anchor-menu.php
+++ includes/enqueue-blocks/anchor-menu.php
@@ -6,53 +6,53 @@
  */
 
 // Exit if accessed directly.
-if (! defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
 // Enqueue AnchorMenu script.
 if (
-	has_block('unityblocks/anchor-menu')
+	has_block( 'unityblocks/anchor-menu' )
 ) {
-	$unity_core_file    = $vendors_url . '/libCore.umd.js';
-	$unity_core_version = UNITYBLOCKS_VERSION . '.' . filemtime($vendors_path . 'libCore.umd.js');
+	$unityblocks_core_file    = $vendors_url . '/libCore.umd.js';
+	$unityblocks_core_version = UNITYBLOCKS_VERSION . '.' . filemtime( $vendors_path . 'libCore.umd.js' );
 
 	wp_enqueue_script(
 		'unity-core',
-		$unity_core_file,
+		$unityblocks_core_file,
 		array( 'wp-element', 'wp-components' ),
-		$unity_core_version,
+		$unityblocks_core_version,
 		true
 	);
 
-	$anchormenu_file    = $dir_url . 'unityblocks-anchormenu.js';
-	$anchormenu_version = UNITYBLOCKS_VERSION . '.' . filemtime($dir_path . 'unityblocks-anchormenu.js');
+	$unityblocks_anchormenu_file    = $dir_url . 'unityblocks-anchormenu.js';
+	$unityblocks_anchormenu_version = UNITYBLOCKS_VERSION . '.' . filemtime( $dir_path . 'unityblocks-anchormenu.js' );
 
 	wp_enqueue_script(
 		'unityblocks-anchormenu',
-		$anchormenu_file,
+		$unityblocks_anchormenu_file,
 		array( 'wp-element', 'wp-components' ),
-		$anchormenu_version,
+		$unityblocks_anchormenu_version,
 		true
 	);
 }
 
 // TODO: test data to pass to frontend React script.
-$anchor_menu_items            = array();
-$anchor_menu_items[]          = array(
+$unityblocks_anchormenu_items   = array();
+$unityblocks_anchormenu_items[] = array(
 	'text'         => 'First container',
 	'targetIdName' => 'first-container',
 	'icon'         => array( 'fas', 'link' ),
 );
-$anchor_menu_items[]          = array(
+$unityblocks_anchormenu_items[] = array(
 	'text'         => 'Second container',
 	'targetIdName' => 'second-container',
 );
-$anchor_menu_items[]          = array(
+$unityblocks_anchormenu_items[] = array(
 	'text'         => 'Third container',
 	'targetIdName' => 'third-container',
 );
-$anchor_menu_items[]          = array(
+$unityblocks_anchormenu_items[] = array(
 	'text'         => 'Fourth container',
 	'targetIdName' => 'fourth-container',
 );
@@ -61,7 +61,7 @@ wp_localize_script(
 	'unityblocks-anchormenu',
 	'unityblocksAnchorMenuData',
 	array(
-		'items'                      => $anchor_menu_items,
+		'items'                      => $unityblocks_anchormenu_items,
 		'firstElementId'             => null,
 		'focusFirstFocusableElement' => null, // bool; not really sure whether to use this.
 	)
