6e096290705c0830466774f8bc776b0fd91e84d7
Nathan Rollins <nathan.rollins@asu.edu>
2022-01-16 16:40:42 -0700

    update frontend enqueing

---
+++ includes/enqueue-blocks/core-components.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Enqueue the frontend scripts for the Anchor Menu block.
+ *
+ * @package UnityBlocks
+ */
+
+// Exit if accessed directly.
+if (! defined('ABSPATH')) {
+    exit;
+}
+
+// Enqueue Core Components scripts.
+if (
+    has_block('unityblocks/anchor-menu')
+) {
+    $unityblocks_core_file    = $vendors_url . '/libCore.umd.js';
+    $unityblocks_core_version = UNITYBLOCKS_VERSION . '.' . filemtime($vendors_path . 'libCore.umd.js');
+
+    wp_enqueue_script(
+        'unity-core',
+        $unityblocks_core_file,
+        array( 'wp-element', 'wp-components' ),
+        $unityblocks_core_version,
+        true
+    );
+
+    $unityblocks_anchormenu_file    = $dir_url . 'unityblocks-anchormenu.js';
+    $unityblocks_anchormenu_version = UNITYBLOCKS_VERSION . '.' . filemtime($dir_path . 'unityblocks-anchormenu.js');
+
+    wp_enqueue_script(
+        'unityblocks-anchormenu',
+        $unityblocks_anchormenu_file,
+        array( 'wp-element', 'wp-components' ),
+        $unityblocks_anchormenu_version,
+        true
+    );
+}
