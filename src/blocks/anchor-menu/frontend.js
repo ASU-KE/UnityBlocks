2974f1d2eebbd702e205581b1e118f38c3ceb916
Nathan Rollins <nathan.rollins@asu.edu>
2022-01-16 16:18:02 -0700

    Refactor frontend logic

--- src/blocks/anchor-menu/frontend.js
+++ src/blocks/anchor-menu/frontend.js
@@ -1,13 +1,27 @@
-const {
-	items,
-	// firstElementId,
-	// focusFirstFocusableElement,
-} = unityblocksAnchorMenuData; // eslint-disable-line no-undef
+/*
+Load the block attributes from the dom element attributes.
+
+If the Unity component was designed to be inserted multiple times into a page, we would select
+the dom elements by the WP-generated class and load the resulting elements into an array.
+The class for this block is wp-block-unityblocks-anchor-menu.
+
+However, the AnchorMenu React component only targets a single occurrence on a page using an element
+id.
+*/
+
+// Load first element with the unityblocks-anchormenu id
+const menu = document.querySelector( '#unityblocks-anchor-menu' );
+
+const items = JSON.parse( menu.dataset.items );
+const firstElementId = menu.dataset.firstElementId;
+const focusFirstFocusableElement = menu.dataset.focusFirstFocusableElement;
 
 // eslint-disable-next-line
 AsuWebCore.initAnchorMenu( {
 	targetSelector: '#unityblocks-anchor-menu',
 	props: {
 		items,
+		firstElementId,
+		focusFirstFocusableElement,
 	},
 } );
--- src/blocks/anchor-menu/save.js
+++ src/blocks/anchor-menu/save.js
@@ -4,12 +4,30 @@
 import classnames from 'classnames';
 
 const save = ( props ) => {
-	const { className } = props;
+	const {
+		attributes: {
+			firstElementId,
+			focusFirstFocusableElement,
+			itemTexts,
+			itemTargets,
+		},
+		className,
+	} = props;
+
+	const items = itemTexts.map( ( itemText, index ) => {
+		return {
+			text: itemText,
+			targetIdName: itemTargets[ index ],
+		};
+	} );
 
 	return (
 		<div
 			id="unityblocks-anchor-menu"
 			className={ classnames( className ) }
+			data-items={ JSON.stringify( items ) }
+			data-firstElementId={ firstElementId }
+			data-focusFirstFocusableElement={ focusFirstFocusableElement }
 		></div>
 	);
 };
