562132fdf962594279c17754ad251ec06187ed61
Nathan Rollins <nathan.rollins@asu.edu>
2022-01-18 21:55:19 -0700

    coding standards

--- includes/class-unityblocks-register-blocks.php
+++ includes/class-unityblocks-register-blocks.php
@@ -6,8 +6,8 @@
  */
 
 // Exit if accessed directly.
-if (! defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
 /**
@@ -15,82 +15,79 @@ if (! defined('ABSPATH')) {
  *
  * @since 1.6.0
  */
-class UnityBlocks_Register_Blocks
-{
+class UnityBlocks_Register_Blocks {
 
 
-    /**
-     * This plugin's instance.
-     *
-     * @var UnityBlocks_Register_Blocks
-     */
-    private static $instance;
 
-    /**
-     * Registers the plugin.
-     *
-     * @return UnityBlocks_Register_Blocks
-     */
-    public static function register()
-    {
-        if (null === self::$instance) {
-            self::$instance = new UnityBlocks_Register_Blocks();
-        }
+	/**
+	 * This plugin's instance.
+	 *
+	 * @var UnityBlocks_Register_Blocks
+	 */
+	private static $instance;
 
-        return self::$instance;
-    }
+	/**
+	 * Registers the plugin.
+	 *
+	 * @return UnityBlocks_Register_Blocks
+	 */
+	public static function register() {
+		if ( null === self::$instance ) {
+			self::$instance = new UnityBlocks_Register_Blocks();
+		}
 
-    /**
-     * The Plugin slug.
-     *
-     * @var string $slug
-     */
-    private $slug;
+		return self::$instance;
+	}
 
-    /**
-     * The Constructor.
-     */
-    public function __construct()
-    {
-        $this->slug = 'unityblocks';
+	/**
+	 * The Plugin slug.
+	 *
+	 * @var string $slug
+	 */
+	private $slug;
 
-        add_action('init', array( $this, 'register_blocks' ), 99);
-    }
+	/**
+	 * The Constructor.
+	 */
+	public function __construct() {
+		$this->slug = 'unityblocks';
 
-    /**
-     * Add actions to enqueue assets.
-     *
-     * @access public
-     */
-    public function register_blocks()
-    {
+		add_action( 'init', array( $this, 'register_blocks' ), 99 );
+	}
 
-        // Return early if this function does not exist.
-        if (! function_exists('register_block_type')) {
-            return;
-        }
+	/**
+	 * Add actions to enqueue assets.
+	 *
+	 * @access public
+	 */
+	public function register_blocks() {
 
-        // Shortcut for the slug.
-        $slug = $this->slug;
+		// Return early if this function does not exist.
+		if ( ! function_exists( 'register_block_type' ) ) {
+			return;
+		}
 
-        register_block_type(
-            $slug . '/anchor-menu',
-            array(
-                'editor_script' => $slug . '-editor',
-                'editor_style'  => $slug . '-editor',
-                'style'         => $slug . '-frontend',
-            )
-        );
+		// Shortcut for the slug.
+		$slug = $this->slug;
 
-        register_block_type(
-            $slug . '/hero',
-            array(
-                'editor_script' => $slug . '-editor',
-                'editor_style'  => $slug . '-editor',
-                'style'         => $slug . '-frontend',
-            )
-        );
-    }
+		register_block_type(
+			$slug . '/anchor-menu',
+			array(
+				'editor_script' => $slug . '-editor',
+				'editor_style'  => $slug . '-editor',
+				'style'         => $slug . '-frontend',
+			)
+		);
+
+		register_block_type(
+			$slug . '/hero',
+			array(
+				'editor_script' => $slug . '-editor',
+				'editor_style'  => $slug . '-editor',
+				'style'         => $slug . '-frontend',
+			)
+		);
+	}
 }
 
 UnityBlocks_Register_Blocks::register();
--- includes/enqueue-blocks/core-components.php
+++ includes/enqueue-blocks/core-components.php
@@ -6,52 +6,52 @@
  */
 
 // Exit if accessed directly.
-if (! defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
 // Enqueue Core Components library.
 if (
-    $this->is_page_gutenberg()
-    || has_block('unityblocks/anchor-menu')
-    || has_block('unityblocks/hero')
-    // || has_block('unityblocks/testimonial')
+	$this->is_page_gutenberg()
+	|| has_block( 'unityblocks/anchor-menu' )
+	|| has_block( 'unityblocks/hero' )
+	// || has_block('unityblocks/testimonial')
 
 ) {
-    $unityblocks_core_file    = $vendors_url . '/libCore.umd.js';
-    $unityblocks_core_version = UNITYBLOCKS_VERSION . '.' . filemtime($vendors_path . 'libCore.umd.js');
+	$unityblocks_core_file    = $vendors_url . '/libCore.umd.js';
+	$unityblocks_core_version = UNITYBLOCKS_VERSION . '.' . filemtime( $vendors_path . 'libCore.umd.js' );
 
-    wp_enqueue_script(
-        'unity-core',
-        $unityblocks_core_file,
-        array( 'wp-element', 'wp-components' ),
-        $unityblocks_core_version,
-        true
-    );
+	wp_enqueue_script(
+		'unity-core',
+		$unityblocks_core_file,
+		array( 'wp-element', 'wp-components' ),
+		$unityblocks_core_version,
+		true
+	);
 }
 
-if ($this->is_page_gutenberg() || has_block('unityblocks/anchor-menu')) {
-    $unityblocks_anchormenu_file    = $dir_url . 'unityblocks-anchormenu.js';
-    $unityblocks_anchormenu_version = UNITYBLOCKS_VERSION . '.' . filemtime($dir_path . 'unityblocks-anchormenu.js');
+if ( $this->is_page_gutenberg() || has_block( 'unityblocks/anchor-menu' ) ) {
+	$unityblocks_anchormenu_file    = $dir_url . 'unityblocks-anchormenu.js';
+	$unityblocks_anchormenu_version = UNITYBLOCKS_VERSION . '.' . filemtime( $dir_path . 'unityblocks-anchormenu.js' );
 
-    wp_enqueue_script(
-        'unityblocks-anchormenu',
-        $unityblocks_anchormenu_file,
-        array( 'wp-element', 'wp-components' ),
-        $unityblocks_anchormenu_version,
-        true
-    );
+	wp_enqueue_script(
+		'unityblocks-anchormenu',
+		$unityblocks_anchormenu_file,
+		array( 'wp-element', 'wp-components' ),
+		$unityblocks_anchormenu_version,
+		true
+	);
 }
 
-if ($this->is_page_gutenberg() || has_block('unityblocks/hero')) {
-    $unityblocks_hero_file    = $dir_url . 'unityblocks-hero.js';
-    $unityblocks_hero_version = UNITYBLOCKS_VERSION . '.' . filemtime($dir_path . 'unityblocks-hero.js');
+if ( $this->is_page_gutenberg() || has_block( 'unityblocks/hero' ) ) {
+	$unityblocks_hero_file    = $dir_url . 'unityblocks-hero.js';
+	$unityblocks_hero_version = UNITYBLOCKS_VERSION . '.' . filemtime( $dir_path . 'unityblocks-hero.js' );
 
-    wp_enqueue_script(
-        'unityblocks-hero',
-        $unityblocks_hero_file,
-        array( 'wp-element', 'wp-components' ),
-        $unityblocks_hero_version,
-        true
-    );
+	wp_enqueue_script(
+		'unityblocks-hero',
+		$unityblocks_hero_file,
+		array( 'wp-element', 'wp-components' ),
+		$unityblocks_hero_version,
+		true
+	);
 }
