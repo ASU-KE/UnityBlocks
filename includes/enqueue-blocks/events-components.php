9bf67d73cb3af76485fdd79e10a3c9f52e16cdbf
Nathan Rollins <nathan.rollins@asu.edu>
2022-01-25 13:56:44 -0700

    Remove enqueuing of *.umd.js vendor scripts

--- includes/enqueue-blocks/events-components.php
+++ includes/enqueue-blocks/events-components.php
@@ -10,25 +10,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// Enqueue Events Components library.
-if (
-	$this->is_page_gutenberg()
-	|| has_block( 'unityblocks/events-grid' )
-	|| has_block( 'unityblocks/events-list' )
-
-) {
-	$unityblocks_core_file    = $vendors_url . '/asuEvents.umd.js';
-	$unityblocks_core_version = UNITYBLOCKS_VERSION . '.' . filemtime( $vendors_path . 'asuEvents.umd.js' );
-
-	wp_enqueue_script(
-		'unityblocks-events-vendor',
-		$unityblocks_core_file,
-		array( 'wp-element', 'wp-components' ),
-		$unityblocks_core_version,
-		true
-	);
-}
-
 if ( $this->is_page_gutenberg() || has_block( 'unityblocks/events-grid' ) ) {
 	$unityblocks_eventsgrid_file    = $dir_url . 'unityblocks-events-grid.js';
 	$unityblocks_eventsgrid_version = UNITYBLOCKS_VERSION . '.' . filemtime( $dir_path . 'unityblocks-events-grid.js' );
--- includes/enqueue-blocks/news-components.php
+++ includes/enqueue-blocks/news-components.php
@@ -10,26 +10,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// Enqueue Core Components library.
-if (
-	$this->is_page_gutenberg()
-	|| has_block( 'unityblocks/news-carousel' )
-	|| has_block( 'unityblocks/news-grid' )
-	|| has_block( 'unityblocks/news-list' )
-
-) {
-	$unityblocks_core_file    = $vendors_url . '/asuNews.umd.js';
-	$unityblocks_core_version = UNITYBLOCKS_VERSION . '.' . filemtime( $vendors_path . 'asuNews.umd.js' );
-
-	wp_enqueue_script(
-		'unityblocks-news-vendor',
-		$unityblocks_core_file,
-		array( 'wp-element', 'wp-components' ),
-		$unityblocks_core_version,
-		true
-	);
-}
-
 if ( $this->is_page_gutenberg() || has_block( 'unityblocks/news-carousel' ) ) {
 	$unityblocks_newscarousel_file    = $dir_url . 'unityblocks-news-carousel.js';
 	$unityblocks_newscarousel_version = UNITYBLOCKS_VERSION . '.' . filemtime( $dir_path . 'unityblocks-news-carousel.js' );
