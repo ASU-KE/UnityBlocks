69d4a6f689d6631fa9074ff91a7a9cee2c7beedd
Nathan Rollins <nathan.rollins@asu.edu>
2022-02-03 19:57:49 -0700

    Refactor Controls to use Tab Panels

--- src/blocks/degree-detail/controls.js
+++ src/blocks/degree-detail/controls.js
@@ -3,14 +3,27 @@
 /**
  * WordPress dependencies
  */
-// import { __ } from '@wordpress/i18n';
-// import {
-// 	BlockControls,
-// 	// MediaPlaceholder,
-// 	MediaUpload,
-// 	MediaUploadCheck,
-// } from '@wordpress/block-editor';
-import { TabPanel } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import {
+	BlockControls,
+	MediaPlaceholder,
+	MediaUpload,
+	MediaUploadCheck,
+} from '@wordpress/block-editor';
+import {
+	IconButton,
+	PanelBody,
+	PanelRow,
+	ResponsiveWrapper,
+	TabPanel,
+	Toolbar,
+} from '@wordpress/components';
+
+/**
+ * Block dependencies
+ */
+
+import icon from './icon';
 
 /**
  * Block controls
@@ -19,8 +32,15 @@ import { TabPanel } from '@wordpress/components';
  */
 const Controls = ( props ) => {
 	const {
-		// attributes: { mediaId },
-		// className,
+		attributes: {
+			heroType,
+			heroMediaId,
+			heroMediaUrl,
+			heroMediaAltText,
+			heroMediaCssClass,
+			heroMediaSize,
+		},
+		className,
 	} = props;
 
 	// const onSelect = ( tabName ) => {};
@@ -29,22 +49,138 @@ const Controls = ( props ) => {
 		<>
 			<TabPanel
 				className="my-tab-panel"
+				orientation={ 'horizontal' }
 				// activeClass="active-tab"
 				// onSelect={ onSelect }
 				tabs={ [
 					{
-						name: 'tab1',
-						title: 'Tab 1',
-						className: 'tab-one',
+						name: 'degree-settings',
+						title: 'Degree',
+						className: '',
 					},
 					{
-						name: 'tab2',
-						title: 'Tab 2',
-						className: 'tab-two',
+						name: 'anchor-menu',
+						title: 'Anchor Menu',
+						className: '',
+					},
+					{
+						name: 'hero-settings',
+						title: 'Hero',
+						className: '',
+					},
+					{
+						name: 'intro-content',
+						title: 'Intro',
+						className: '',
+					},
+					{
+						name: 'hide-sections',
+						title: 'Hide Sections',
+						className: '',
+					},
+					{
+						name: 'override-images',
+						title: 'Override images',
+						className: '',
+					},
+					{
+						name: 'at-a-glance',
+						title: 'At a Glance',
+						className: '',
+					},
+					{
+						name: 'program-contacts',
+						title: 'Program Contacts',
+						className: '',
+					},
+					{
+						name: 'next-steps',
+						title: 'Next steps',
+						className: '',
+					},
+					{
+						name: 'why-choose-asu',
+						title: 'Why ASU',
+						className: '',
 					},
 				] }
 			>
-				{ ( tab ) => <p>Selected tab: { tab.title }</p> }
+				{ ( tab ) => {
+					if ( 'hero-settings' === tab.name ) {
+						const onSelectHeroMedia = ( media ) => {
+							props.setAttributes( {
+								heroMediaId: media.id,
+								heroMediaUrl: media.url,
+								heroMediaAltText: media.alt,
+							} );
+						};
+
+						return (
+							<>
+								{ heroMediaId !== 0 && (
+									<BlockControls>
+										<Toolbar>
+											<MediaUploadCheck>
+												<MediaUpload
+													value={ heroMediaId }
+													onSelect={
+														onSelectHeroMedia
+													}
+													allowedTypes={ [ 'image' ] }
+													render={ ( { open } ) => (
+														<IconButton
+															className="components-toolbar__control"
+															label={ __(
+																'Edit image',
+																'unityblocks'
+															) }
+															icon="edit"
+															onClick={ open }
+														/>
+													) }
+												/>
+											</MediaUploadCheck>
+										</Toolbar>
+									</BlockControls>
+								) }
+								{ heroMediaId === 0 && (
+									<div className={ `${ className }` }>
+										<MediaUploadCheck>
+											<MediaPlaceholder
+												labels={ {
+													title: __(
+														'Hero image',
+														'unityblocks'
+													),
+													instructions: __(
+														'Drag image, upload a new one or select file from your library.',
+														'unityblocks'
+													),
+												} }
+												icon={ icon }
+												accept="images/*"
+												onSelect={ onSelectHeroMedia }
+											/>
+										</MediaUploadCheck>
+									</div>
+								) }
+								{ heroMediaId !== 0 && (
+									<ResponsiveWrapper
+										naturalWidth={ 1920 }
+										naturalHeight={ 512 }
+									>
+										<img
+											alt={ heroMediaAltText }
+											src={ heroMediaUrl }
+										/>
+									</ResponsiveWrapper>
+								) }
+							</>
+						);
+					}
+
+					return <p>Placeholder</p>;
+				} }
 			</TabPanel>
 		</>
 	);
