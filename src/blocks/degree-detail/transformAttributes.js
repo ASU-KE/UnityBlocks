21d78922cfb889aa1c7b0d5bfe4b544fb24fbf44
Nathan Rollins <nathan.rollins@asu.edu>
2022-02-11 23:01:04 -0700

    Add attributes for Why Choose ASU cards

--- src/blocks/degree-detail/transformAttributes.js
+++ src/blocks/degree-detail/transformAttributes.js
@@ -94,6 +94,66 @@ const transformAttributes = ( attributes ) => {
 		nextSteps__cardsButtonSize,
 		whyChooseAsu__hide,
 		whyChooseAsu__sectionIntroText,
+		whyChooseAsu__facultyImageUrl,
+		whyChooseAsu__facultyImageAltText,
+		whyChooseAsu__facultyImageCss,
+		whyChooseAsu__facultyTitle,
+		whyChooseAsu__facultyText,
+		whyChooseAsu__facultyButtonIcon,
+		whyChooseAsu__facultyButtonLabel,
+		whyChooseAsu__facultyButtonUrl,
+		whyChooseAsu__facultyButtonColor,
+		whyChooseAsu__facultyButtonSize,
+		whyChooseAsu__programsImageUrl,
+		whyChooseAsu__programsImageAltText,
+		whyChooseAsu__programsImageCss,
+		whyChooseAsu__programsTitle,
+		whyChooseAsu__programsText,
+		whyChooseAsu__programsButtonIcon,
+		whyChooseAsu__programsButtonLabel,
+		whyChooseAsu__programsButtonUrl,
+		whyChooseAsu__programsButtonColor,
+		whyChooseAsu__programsButtonSize,
+		whyChooseAsu__researchImageUrl,
+		whyChooseAsu__researchImageAltText,
+		whyChooseAsu__researchImageCss,
+		whyChooseAsu__researchTitle,
+		whyChooseAsu__researchText,
+		whyChooseAsu__researchButtonIcon,
+		whyChooseAsu__researchButtonLabel,
+		whyChooseAsu__researchButtonUrl,
+		whyChooseAsu__researchButtonColor,
+		whyChooseAsu__researchButtonSize,
+		whyChooseAsu__inclusionImageUrl,
+		whyChooseAsu__inclusionImageAltText,
+		whyChooseAsu__inclusionImageCss,
+		whyChooseAsu__inclusionTitle,
+		whyChooseAsu__inclusionText,
+		whyChooseAsu__inclusionButtonIcon,
+		whyChooseAsu__inclusionButtonLabel,
+		whyChooseAsu__inclusionButtonUrl,
+		whyChooseAsu__inclusionButtonColor,
+		whyChooseAsu__inclusionButtonSize,
+		whyChooseAsu__mentorsImageUrl,
+		whyChooseAsu__mentorsImageAltText,
+		whyChooseAsu__mentorsImageCss,
+		whyChooseAsu__mentorsTitle,
+		whyChooseAsu__mentorsText,
+		whyChooseAsu__mentorsButtonIcon,
+		whyChooseAsu__mentorsButtonLabel,
+		whyChooseAsu__mentorsButtonUrl,
+		whyChooseAsu__mentorsButtonColor,
+		whyChooseAsu__mentorsButtonSize,
+		whyChooseAsu__honorsImageUrl,
+		whyChooseAsu__honorsImageAltText,
+		whyChooseAsu__honorsImageCss,
+		whyChooseAsu__honorsTitle,
+		whyChooseAsu__honorsText,
+		whyChooseAsu__honorsButtonIcon,
+		whyChooseAsu__honorsButtonLabel,
+		whyChooseAsu__honorsButtonUrl,
+		whyChooseAsu__honorsButtonColor,
+		whyChooseAsu__honorsButtonSize,
 	} = attributes;
 
 	const dataSource = {
@@ -130,7 +190,9 @@ const transformAttributes = ( attributes ) => {
 						? {
 								url: heroMediaUrl,
 								altText: heroMediaAltText,
-								cssClass: heroMediaCssClass.split( ' ' ),
+								cssClass: heroMediaCssClass
+									? heroMediaCssClass.split( ' ' )
+									: undefined,
 								size: heroMediaSize,
 						  }
 						: undefined,
@@ -140,7 +202,9 @@ const transformAttributes = ( attributes ) => {
 								text: heroTitleText,
 								highlightColor: heroTitleHighlightColor,
 								maxWidth: heroTitleMaxWidth,
-								cssClass: heroTitleCssClass.split( ' ' ),
+								cssClass: heroTitleCssClass
+									? heroTitleCssClass.split( ' ' )
+									: undefined,
 						  }
 						: undefined,
 				subTitle:
@@ -149,7 +213,9 @@ const transformAttributes = ( attributes ) => {
 								text: heroSubTitleText,
 								highlightColor: heroSubTitleHighlightColor,
 								maxWidth: heroSubTitleMaxWidth,
-								cssClass: heroSubTitleCssClass.split( ' ' ),
+								cssClass: heroSubTitleCssClass
+									? heroSubTitleCssClass.split( ' ' )
+									: undefined,
 						  }
 						: undefined,
 				contents:
@@ -159,7 +225,9 @@ const transformAttributes = ( attributes ) => {
 									text: heroContentsText,
 									highlightColor: heroContentsHighlightColor,
 									maxWidth: heroContentsMaxWidth,
-									cssClass: heroContentsCssClass.split( ' ' ),
+									cssClass: heroContentsCssClass
+										? heroContentsCssClass.split( ' ' )
+										: undefined,
 								},
 						  ]
 						: undefined,
@@ -198,7 +266,9 @@ const transformAttributes = ( attributes ) => {
 		? {
 				url: introContent__imageUrl,
 				altText: introContent__imageAltText,
-				cssClass: introContent__imageCssClass.split( ' ' ),
+				cssClass: introContent__imageCssClass
+					? introContent__imageCssClass.split( ' ' )
+					: undefined,
 		  }
 		: undefined;
 
@@ -250,7 +320,9 @@ const transformAttributes = ( attributes ) => {
 				? {
 						url: careerOutlook__imageUrl,
 						text: careerOutlook__imageAltText,
-						cssClass: careerOutlook__imageCssClass.split( ' ' ),
+						cssClass: careerOutlook__imageCssClass
+							? careerOutlook__imageCssClass.split( ' ' )
+							: undefined,
 				  }
 				: undefined,
 		},
@@ -261,7 +333,9 @@ const transformAttributes = ( attributes ) => {
 				? {
 						url: globalOpportunity__imageUrl,
 						altText: globalOpportunity__imageAltText,
-						cssClass: globalOpportunity__imageCssClass.split( ' ' ),
+						cssClass: globalOpportunity__imageCssClass
+							? globalOpportunity__imageCssClass.split( ' ' )
+							: undefined,
 				  }
 				: undefined,
 		},
@@ -271,7 +345,9 @@ const transformAttributes = ( attributes ) => {
 				? {
 						url: attendOnline__imageUrl,
 						altText: attendOnline__imageAltText,
-						cssClass: attendOnline__imageCssClass.split( ' ' ),
+						cssClass: attendOnline__imageCssClass
+							? attendOnline__imageCssClass.split( ' ' )
+							: undefined,
 				  }
 				: undefined,
 		},
@@ -302,6 +378,116 @@ const transformAttributes = ( attributes ) => {
 		whyChooseAsu: {
 			hide: whyChooseAsu__hide,
 			sectionIntroText: whyChooseAsu__sectionIntroText,
+			cards: {
+				faculty: whyChooseAsu__facultyTitle
+					? {
+							image: {
+								url: whyChooseAsu__facultyImageUrl,
+								altText: whyChooseAsu__facultyImageAltText,
+								cssClass: whyChooseAsu__facultyImageCss,
+							},
+							title: whyChooseAsu__facultyTitle,
+							text: whyChooseAsu__facultyText,
+							button: {
+								icon: whyChooseAsu__facultyButtonIcon,
+								label: whyChooseAsu__facultyButtonLabel,
+								url: whyChooseAsu__facultyButtonUrl,
+								color: whyChooseAsu__facultyButtonColor,
+								size: whyChooseAsu__facultyButtonSize,
+							},
+					  }
+					: undefined,
+				programs: whyChooseAsu__programsTitle
+					? {
+							image: {
+								url: whyChooseAsu__programsImageUrl,
+								altText: whyChooseAsu__programsImageAltText,
+								cssClass: whyChooseAsu__programsImageCss,
+							},
+							title: whyChooseAsu__programsTitle,
+							text: whyChooseAsu__programsText,
+							button: {
+								icon: whyChooseAsu__programsButtonIcon,
+								label: whyChooseAsu__programsButtonLabel,
+								url: whyChooseAsu__programsButtonUrl,
+								color: whyChooseAsu__programsButtonColor,
+								size: whyChooseAsu__programsButtonSize,
+							},
+					  }
+					: undefined,
+				research: whyChooseAsu__researchTitle
+					? {
+							image: {
+								url: whyChooseAsu__researchImageUrl,
+								altText: whyChooseAsu__researchImageAltText,
+								cssClass: whyChooseAsu__researchImageCss,
+							},
+							title: whyChooseAsu__researchTitle,
+							text: whyChooseAsu__researchText,
+							button: {
+								icon: whyChooseAsu__researchButtonIcon,
+								label: whyChooseAsu__researchButtonLabel,
+								url: whyChooseAsu__researchButtonUrl,
+								color: whyChooseAsu__researchButtonColor,
+								size: whyChooseAsu__researchButtonSize,
+							},
+					  }
+					: undefined,
+				inclusion: whyChooseAsu__inclusionTitle
+					? {
+							image: {
+								url: whyChooseAsu__inclusionImageUrl,
+								altText: whyChooseAsu__inclusionImageAltText,
+								cssClass: whyChooseAsu__inclusionImageCss,
+							},
+							title: whyChooseAsu__inclusionTitle,
+							text: whyChooseAsu__inclusionText,
+							button: {
+								icon: whyChooseAsu__inclusionButtonIcon,
+								label: whyChooseAsu__inclusionButtonLabel,
+								url: whyChooseAsu__inclusionButtonUrl,
+								color: whyChooseAsu__inclusionButtonColor,
+								size: whyChooseAsu__inclusionButtonSize,
+							},
+					  }
+					: undefined,
+				mentors: whyChooseAsu__mentorsTitle
+					? {
+							image: {
+								url: whyChooseAsu__mentorsImageUrl,
+								altText: whyChooseAsu__mentorsImageAltText,
+								cssClass: whyChooseAsu__mentorsImageCss,
+							},
+							title: whyChooseAsu__mentorsTitle,
+							text: whyChooseAsu__mentorsText,
+							button: {
+								icon: whyChooseAsu__mentorsButtonIcon,
+								label: whyChooseAsu__mentorsButtonLabel,
+								url: whyChooseAsu__mentorsButtonUrl,
+								color: whyChooseAsu__mentorsButtonColor,
+								size: whyChooseAsu__mentorsButtonSize,
+							},
+					  }
+					: undefined,
+				honors: whyChooseAsu__honorsTitle
+					? {
+							image: {
+								url: whyChooseAsu__honorsImageUrl,
+								altText: whyChooseAsu__honorsImageAltText,
+								cssClass: whyChooseAsu__honorsImageCss,
+							},
+							title: whyChooseAsu__honorsTitle,
+							text: whyChooseAsu__honorsText,
+							button: {
+								icon: whyChooseAsu__honorsButtonIcon,
+								label: whyChooseAsu__honorsButtonLabel,
+								url: whyChooseAsu__honorsButtonUrl,
+								color: whyChooseAsu__honorsButtonColor,
+								size: whyChooseAsu__honorsButtonSize,
+							},
+					  }
+					: undefined,
+			},
 		},
 	};
 };
