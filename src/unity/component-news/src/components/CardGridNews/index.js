c90cb0fa96c87bb4cc1286e12adfd1d2116a911b
Nathan Rollins <nathan.rollins@asu.edu>
2022-04-25 18:33:38 -0700

    Refactor unity component-news grid component

--- src/unity/component-news/src/components/CardGridNews/index.js
+++ src/unity/component-news/src/components/CardGridNews/index.js
@@ -14,33 +14,46 @@ import { NewsWrapper } from './index.styles';
 /**
  *
  * @param {Object} story
+ * @param {Boolean} enableStoryDate
  * @param {import("../../core/types/news-types").CardButton} cardButton
  */
-const gridRow = ( story, cardButton ) => (
+const storyBody = ( story, enableStoryDate ) => {
+	return enableStoryDate
+		? `<p><b>${ story.date }</b></p><p class="card-text text-dark">${ story.content }</p>`
+		: `<p class="card-text text-dark">${ story.content }</p>`;
+};
+
+/**
+ *
+ * @param {Object} story
+ * @param {Boolean} enableCardTags
+ * @param {Boolean} enableStoryDate
+ * @param {import("../../core/types/news-types").CardButton} cardButton
+ */
+const gridRow = ( story, cardButton, enableCardTags, enableStoryDate ) => (
 	<div
 		className="col col-12 col-md-6 col-lg-4 cards-items-container"
 		key={ story.id }
 	>
 		<Card
-			type="default"
-			clickable={ !! story.buttonLink }
-			clickHref={ story.buttonLink }
+			type="story"
+			clickable={ false }
 			title={ story.title }
-			body={ `<p class="card-text text-dark">${ story.content }</p>` }
+			body={ storyBody( story, enableStoryDate ) }
 			image={ story.imageUrl }
 			imageAltText={ story.title }
-			linkLabel={ story.eventButtonText }
-			linkUrl={ story.eventButtonUrl }
-			buttons={ [
-				{
-					ariaLabel: cardButton.text,
-					color: cardButton.color,
-					label: cardButton.text,
-					size: cardButton.size,
-					href: story.buttonLink,
-				},
-			] }
-			tags={ parseInterests( story?.interests ) }
+			linkLabel={ 'Read story' }
+			linkUrl={ story.buttonLink }
+			// buttons={ [
+			// 	{
+			// 		ariaLabel: cardButton.text,
+			// 		color: cardButton.color,
+			// 		label: cardButton.text,
+			// 		size: cardButton.size,
+			// 		href: story.buttonLink,
+			// 	},
+			// ] }
+			tags={ enableCardTags ? parseInterests( story?.interests ) : null }
 		/>
 	</div>
 );
@@ -49,7 +62,7 @@ const gridRow = ( story, cardButton ) => (
  * @param {import("../../core/types/news-types").TemplateProps} props
  */
 // eslint-disable-next-line react/prop-types
-const GridTemplate = ( { cardButton } ) => {
+const GridTemplate = ( { cardButton, enableCardTags, enableStoryDate } ) => {
 	const { stories } = useContext( FeedContext ); // Reading the "stories" object from the context
 
 	return (
@@ -60,7 +73,12 @@ const GridTemplate = ( { cardButton } ) => {
 			{ stories?.map( ( story, index ) => (
 				// eslint-disable-next-line react/no-array-index-key
 				<React.Fragment key={ index }>
-					{ gridRow( story, cardButton ) }
+					{ gridRow(
+						story,
+						cardButton,
+						enableCardTags,
+						enableStoryDate
+					) }
 				</React.Fragment>
 			) ) }
 		</NewsWrapper>
@@ -75,11 +93,18 @@ const GridTemplate = ( { cardButton } ) => {
 /**
  * @param {FeedType} props
  */
-const CardGridNews = ( { cardButton, ...props } ) => (
+const CardGridNews = ( {
+	cardButton,
+	enableCardTags,
+	enableStoryDate,
+	...props
+} ) => (
 	// Calling the high order component that fetch the data
 	<BaseFeed { ...props }>
 		<GridTemplate
 			cardButton={ { ...defaultProps.cardButton, ...cardButton } }
+			enableCardTags={ enableCardTags }
+			enableStoryDate={ enableStoryDate }
 		/>
 	</BaseFeed>
 );
