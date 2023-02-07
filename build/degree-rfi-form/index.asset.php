137e3f505f52685d7f3500cceb01a95ba7b22b4d
John Cox <john.k.cox@asu.edu>
2023-02-07 14:57:26 -0700

    fix json file

--- build/degree-rfi-form/index.asset.php
+++ build/degree-rfi-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '64160a9c958b11d64e7a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '58e303a2f0798920064d');
