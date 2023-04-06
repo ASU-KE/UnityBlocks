3813b187080e6da877980cd52aa497b2c09b0b0a
John Cox <john.k.cox@asu.edu>
2023-04-06 13:15:34 -0700

    refactor asu-careers into v2, add resources, .gitignore update

---
+++ src/asu-careers/edit.js
@@ -0,0 +1,33 @@
+import { AsuCareers } from './components/AsuCareers';
+
+/**
+ * Internal dependencies
+ */
+import Inspector from './inspector';
+
+/**
+ * Block edit function
+ *
+ * @param {Object} props
+ */
+const Edit = ( props ) => {
+	const {
+		attributes: { listType, deptList, titleText },
+		className,
+	} = props;
+
+	return (
+		<>
+			<Inspector { ...props } />
+			<div className={ className }>
+				<AsuCareers
+					editorListType={ listType }
+					deptList={ deptList }
+					titleText={ titleText }
+				/>
+			</div>
+		</>
+	);
+};
+
+export default Edit;
