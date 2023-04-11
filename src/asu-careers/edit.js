b9b3a1702cd35dc1382b0656d501ac6b0ccf682a
John Cox <john.k.cox@asu.edu>
2023-04-11 13:51:03 -0700

    edit useBlockProps, scss files

--- src/asu-careers/edit.js
+++ src/asu-careers/edit.js
@@ -1,25 +1,48 @@
-import { AsuCareers } from '../../resources/unity/component-careers/AsuCareers';
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from "@wordpress/i18n";
 
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import { useBlockProps } from "@wordpress/block-editor";
+/**
+ * External dependencies
+ */
+import { AsuCareers } from '../../resources/unity/component-careers/AsuCareers';
 /**
  * Internal dependencies
  */
 import Inspector from './inspector';
+import './editor.scss';
 
 /**
- * Block edit function
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
  *
- * @param {Object} props
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
  */
 const Edit = ( props ) => {
 	const {
-		attributes: { listType, deptList, titleText },
-		className,
+		attributes: {
+      listType,
+      deptList,
+      titleText,
+    },
 	} = props;
 
 	return (
 		<>
 			<Inspector { ...props } />
-			<div className={ className }>
+			<div {...useBlockProps()}>
 				<AsuCareers
 					editorListType={ listType }
 					deptList={ deptList }
