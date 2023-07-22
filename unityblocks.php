b36dc50ad49e8349275c08895db20ab9701b986c
Nathan Rollins <nathan.rollins@asu.edu>
2023-07-21 17:50:01 -0700

    Add Git Updater metadata

--- unityblocks.php
+++ unityblocks.php
@@ -11,6 +11,8 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       unityblocks
  *
+ * GitHub Plugin URI: ASU-KE/UnityBlocks
+ *
  * @package           unityblocks
  */
 
