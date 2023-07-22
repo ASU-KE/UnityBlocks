e6d2d891ce29b4c20c43ef2d56632bb46241a080
Nathan Rollins <nathan.rollins@asu.edu>
2023-07-21 17:24:37 -0700

    Update webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const { merge } = require("webpack-merge");
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+
+module.exports = merge(defaultConfig, {
+  plugins: [
+    // new CopyPlugin({
+    //   patterns: [
+    //     {
+    //       from: "dist/",
+    //       context: "node_modules/@asu/app-webdir-ui/",
+    //       to: path.resolve(__dirname, "resources/unity-import/app-webdir-ui/"),
+    //     },
+    //     {
+    //       from: "dist/",
+    //       context: "node_modules/@asu/components-core/",
+    //       to: path.resolve(__dirname, "resources/unity-import/components-core/"),
+    //     },
+    //   ],
+    // }),
+  ],
+  resolve: {
+    alias: {
+      Unity: path.resolve(__dirname, "resources/unity-import/"),
+    },
+  },
+});
