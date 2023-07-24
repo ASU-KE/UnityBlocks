94164a8dc674a7d50a205311b42a72af8774c1a3
Nathan Rollins <nathan.rollins@asu.edu>
2023-07-23 22:39:29 -0700

    Add logic for Pitchfork theme

--- unityblocks.php
+++ unityblocks.php
@@ -23,6 +23,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
+ * Load current theme and check if it is Pitchfork or a Pitchfork Child
+ */
+$theme_data = wp_get_theme();
+$pitchfork_theme = ( 'pitchfork' === $theme_data->get( 'TextDomain' ) || 'pitchfork' === $theme_data->get( 'Template' ) );
+
+
+/**
  * Registers the block using the metadata loaded from the `block.json` file.
  * Behind the scenes, it also registers all assets so they can be enqueued
  * through the block editor in the corresponding context.
@@ -30,6 +37,12 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 function unityblocks_block_init() {
+	// Register these blocks only if not using Pitchfork
+	// Pitchfork already has these blocks
+	if ( $pitchfork_theme ) {
+		register_block_type( __DIR__ . '/build/hero' );
+	}
+
 	register_block_type( __DIR__ . '/build/anchor-menu' );
 	register_block_type( __DIR__ . '/build/hero' );
 	register_block_type( __DIR__ . '/build/events-grid' );
