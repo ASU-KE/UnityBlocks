49c84f817661ede17afd00db9127faba6ce56722
“ZainabD” <zalsidik@asu.edu>
2023-09-06 15:23:19 -0700

    updates to asu-careers when running npm run build

--- build/asu-careers/index.asset.php
+++ build/asu-careers/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '772eb7e40cda7d892ccb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '861eed098a7f45d2999b');
