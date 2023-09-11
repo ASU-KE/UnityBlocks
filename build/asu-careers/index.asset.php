c6ed775e9b5da2727e156215987f2d53d65e24c7
“ZainabD” <zalsidik@asu.edu>
2023-09-11 15:26:19 -0700

    added new departments to the career list per request IR346945

--- build/asu-careers/index.asset.php
+++ build/asu-careers/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e50069eabe02d2e0a978');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'eb34fb5425c0960a951f');
