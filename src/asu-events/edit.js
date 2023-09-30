3932c47293a13a9e49f33f4421f06cbf7176ccaa
Nathan Rollins <nathan.rollins@asu.edu>
2023-09-29 19:40:31 -0700

    Add asu-events block

---
+++ src/asu-events/edit.js
@@ -0,0 +1,97 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from "@wordpress/i18n";
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import { useBlockProps } from "@wordpress/block-editor";
+
+/**
+ * External dependencies
+ */
+import { formatISO, startOfToday } from "date-fns";
+import { CardsGridEvents } from "../../resources/unity/component-events/src/components/CardsGridEvents";
+
+/**
+ * Internal dependencies
+ */
+import Inspector from "./inspector";
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+// import "./editor.scss";
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
+ */
+const Edit = (props) => {
+  const {
+    attributes: {
+      enableHeader,
+      headerText,
+      headerColor,
+      ctaColor,
+      ctaText,
+      ctaUrl,
+      dataSourceUrl,
+      dataSourceFilters,
+      noResultsText,
+      maxItems,
+    },
+  } = props;
+
+  const header = enableHeader
+    ? {
+        color: headerColor,
+        text: headerText,
+      }
+    : null;
+
+  const ctaButton = enableHeader
+    ? {
+        color: ctaColor,
+        text: ctaText,
+        url: ctaUrl,
+      }
+    : null;
+
+  const dataSource = {
+    url: dataSourceUrl,
+    filters: dataSourceFilters,
+  };
+
+  const args = {
+    header,
+    ctaButton,
+    dataSource,
+    noResultsText,
+    maxItems,
+  };
+
+  return (
+    <>
+      <Inspector {...props} />
+      <div {...useBlockProps()}>
+        <CardsGridEvents {...args} />
+      </div>
+    </>
+  );
+};
+
+export default Edit;
---
+++ src/asu-events/inspector.js
@@ -0,0 +1,189 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from "@wordpress/i18n";
+import { InspectorControls } from "@wordpress/block-editor";
+import {
+  PanelBody,
+  PanelRow,
+  RadioControl,
+  SelectControl,
+  TextControl,
+  ToggleControl,
+} from "@wordpress/components";
+import { useFetchKeGraphql } from "../../resources/unity/components-core/src/core/hooks/use-fetch-ke-graphql";
+import { LIST_GROUPS_QUERY } from "../../resources/unity/component-events/src/core/graphql";
+
+/**
+ * Inspector controls
+ *
+ * @param {Object} props
+ */
+const Inspector = (props) => {
+  const {
+    attributes: {
+      enableHeader,
+      headerText,
+      headerColor,
+      ctaColor,
+      ctaText,
+      ctaUrl,
+      dataSourceUrl,
+      dataSourceFilters,
+      noResultsText,
+      maxItems,
+    },
+    setAttributes,
+  } = props;
+
+  return (
+    <>
+      <InspectorControls>
+        <PanelBody title={__("Header", "unityblocks")} initialOpen={true}>
+          <PanelRow>
+            <ToggleControl
+              label={"Enable Header"}
+              help={enableHeader ? "Header enabled." : "Header disabled."}
+              checked={enableHeader}
+              onChange={(value) => {
+                setAttributes({ enableHeader: value });
+              }}
+            />
+          </PanelRow>
+          {enableHeader && (
+            <>
+              <PanelRow>
+                <TextControl
+                  label={"Header text"}
+                  value={headerText}
+                  onChange={(value) => setAttributes({ headerText: value })}
+                />
+              </PanelRow>
+              <PanelRow>
+                <RadioControl
+                  label={__("Header text color", "unityblocks")}
+                  selected={headerColor}
+                  options={[
+                    {
+                      label: __("White", "unityblocks"),
+                      value: "white",
+                    },
+                    {
+                      label: __("Dark", "unityblocks"),
+                      value: "dark",
+                    },
+                  ]}
+                  onChange={(value) => setAttributes({ headerColor: value })}
+                />
+              </PanelRow>
+            </>
+          )}
+        </PanelBody>
+
+        {enableHeader && (
+          <PanelBody
+            title={__("CTA button", "unityblocks")}
+            initialOpen={false}
+          >
+            <PanelRow>
+              <TextControl
+                label={"CTA text"}
+                placeholder="Click to see more events"
+                value={ctaText}
+                onChange={(value) => setAttributes({ ctaText: value })}
+              />
+            </PanelRow>
+            <PanelRow>
+              <TextControl
+                className="eventsgrid__cardbutton-url"
+                label={"CTA URL"}
+                value={ctaUrl}
+                onChange={(value) => setAttributes({ ctaUrl: value })}
+              />
+            </PanelRow>
+            <PanelRow>
+              <RadioControl
+                label={__("CTA button color", "unityblocks")}
+                selected={ctaColor}
+                options={[
+                  {
+                    label: __("Gold", "unityblocks"),
+                    value: "gold",
+                  },
+                  {
+                    label: __("Maroon", "unityblocks"),
+                    value: "maroon",
+                  },
+                  {
+                    label: __("Gray", "unityblocks"),
+                    value: "gray",
+                  },
+                  {
+                    label: __("Dark", "unityblocks"),
+                    value: "dark",
+                  },
+                ]}
+                onChange={(value) => setAttributes({ ctaColor: value })}
+              />
+            </PanelRow>
+          </PanelBody>
+        )}
+
+        <PanelBody>
+          <PanelRow>
+            <TextControl
+              label={"Text to display when no results found"}
+              value={noResultsText}
+              onChange={(value) => setAttributes({ noResultsText: value })}
+            />
+          </PanelRow>
+          <PanelRow>
+            <TextControl
+              label={"Max items to load"}
+              help={
+                "Changing this value doesn't update the Edit view immediately. Update and reload to refresh the editor."
+              }
+              value={maxItems}
+              onChange={(value) =>
+                setAttributes({
+                  maxItems: Number(value), // Force attribute to number because this input field returns value as string.
+                })
+              }
+            />
+          </PanelRow>
+        </PanelBody>
+
+        <PanelBody title={__("Data Source", "unityblocks")} initialOpen={false}>
+          <PanelRow>
+            <TextControl
+              label={"Url"}
+              help={"Data source url requires CORS proxy."}
+              value={dataSourceUrl}
+              onChange={(value) =>
+                setAttributes({
+                  dataSourceUrl: value,
+                })
+              }
+            />
+          </PanelRow>
+          <PanelRow>
+            <TextControl
+              label={"Events filters"}
+              help={
+                "Enter taxonomy terms, separated by commas. For the master list of available filtering terms, refer to: https://asuevents.asu.edu/reports/taxonomy-terms"
+              }
+              value={dataSourceFilters}
+              onChange={(value) =>
+                setAttributes({
+                  dataSourceFilters: value,
+                })
+              }
+            />
+          </PanelRow>
+        </PanelBody>
+      </InspectorControls>
+    </>
+  );
+};
+
+export default Inspector;
