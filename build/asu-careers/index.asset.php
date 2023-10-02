2262b91c3ca0d565def5e3269c3c75c56b367317
“ZainabD” <zalsidik@asu.edu>
2023-10-02 14:14:22 -0700

    updated the career block style on mobile size

--- build/asu-careers/index.asset.php
+++ build/asu-careers/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'eb34fb5425c0960a951f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1002d96f73ebfdb369ce');
