c9a2fcd275d84407547ce82d95a48d4da16df76f
John Cox <john.k.cox@asu.edu>
2023-12-07 11:10:07 -0700

    rebuild w/ latest pkgs

--- build/asu-events/frontend.asset.php
+++ build/asu-events/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '490f7371ed60108a6da0');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '9c29181890285eb67896');
