ead5bbd644f15dd53c12a80ffb0d8d52fc50ad0c
John Cox <john.k.cox@asu.edu>
2023-12-07 11:46:53 -0700

    build

--- build/news-grid/frontend.asset.php
+++ build/news-grid/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-element'), 'version' => '5aba72b80469fcdb98e0');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-element'), 'version' => '504d273908b73d627461');
