125601ff0d263e388ce6b1fa5d1a150c43adef94
Nathan Rollins <nathan.rollins@asu.edu>
2023-12-19 10:28:59 -0700

    npm run build

--- build/wchm/index.asset.php
+++ build/wchm/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a73d8501b58a22bbeaeb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9b12429866b93042b260');
