55fb0c23b75ce98db1d414f18072fbe3d7d48b46
“ZainabD” <zalsidik@asu.edu>
2024-02-19 16:07:00 -0700

    updated the brassring fetch url to the proxy-manager -2

--- build/asu-careers/frontend.asset.php
+++ build/asu-careers/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'be69c665562a723a298a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'e4706ce8cb0668add7c2');
