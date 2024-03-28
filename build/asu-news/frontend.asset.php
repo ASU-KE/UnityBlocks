10af65903074c933950bcb672afeb4e164278118
John Cox <john.k.cox@asu.edu>
2024-03-28 15:14:09 -0700

    build v1.0.0 asu-news

---
+++ build/asu-news/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-element'), 'version' => 'b2a8ef962251a750b3aa');
---
+++ build/asu-news/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b57acd2175e0ec03fe0a');
