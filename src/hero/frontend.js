1f44f738ea979ab74393a8495eafae70908e095a
John Cox <john.k.cox@asu.edu>
2024-11-19 16:33:17 -0700

    createRoot refactor

--- src/hero/frontend.js
+++ src/hero/frontend.js
@@ -1,24 +1,23 @@
-const { render } = wp.element;
-
+import { createRoot } from 'react-dom/client'
 import { Hero } from "../../resources/asu-unity-stack/packages/components-core/src/components/Hero"
-// Load first element with the unityblocks-hero id
-const hero = document.querySelector("#unityblocks-hero");
 
-const type = hero.dataset.herotype;
-const image = JSON.parse(hero.dataset.image);
-const subTitle = JSON.parse(hero.dataset.subtitle);
-const title = JSON.parse(hero.dataset.title);
-const contents = JSON.parse(hero.dataset.contents);
-const contentsColor = hero.dataset.contentscolor;
+const hero = document.getElementById("unityblocks-hero")
+const root = createRoot(hero)
 
-render(
-  <Hero
-    type={type}
-    image={image}
-    subTitle={subTitle}
-    title={title}
-    contents={contents}
-    contentsColor={contentsColor}
-  />,
-  hero
-);
+const type = hero.dataset.herotype
+const image = JSON.parse(hero.dataset.image)
+const subTitle = JSON.parse(hero.dataset.subtitle)
+const title = JSON.parse(hero.dataset.title)
+const contents = JSON.parse(hero.dataset.contents)
+const contentsColor = hero.dataset.contentscolor
+
+const props = {
+  type: type,
+  image: image,
+  subTitle: subTitle,
+  title: title,
+  contents: contents,
+  contentsColor: contentsColor
+}
+
+root.render(<Hero { ...props}/>)
