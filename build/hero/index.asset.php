56dc1fe7015aacf224382eacfc2628dad4d6b7df
John Cox <john.k.cox@asu.edu>
2024-11-19 17:04:21 -0700

    build latest

--- build/hero/index.asset.php
+++ build/hero/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a43b23ce1ef09c624aad');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4a78a6d2e9de5adaf64d');
