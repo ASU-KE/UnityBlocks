a2d5e1e4b9018fdadba73590f86a323838169040
John Cox <john.k.cox@asu.edu>
2025-06-19 16:02:00 -0700

    v3.0.0 - uds overhaul

--- unityblocks.php
+++ unityblocks.php
@@ -5,7 +5,7 @@
  * Description:       UnityBlocks is a suite of page building content blocks for the ASU Web Standards Unity (UDS) WordPress theme.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           2.4
+ * Version:           3.0.0
  * Author:            ASU KE Web Services
  * Author URI:        https://rto.asu.edu/web-services
  * License:           GPL-2.0-or-later
