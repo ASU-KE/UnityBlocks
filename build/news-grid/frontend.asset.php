8ebf4282ae344ce65f78f283e0f28b857fa87779
John Cox <john.k.cox@asu.edu>
2025-06-20 13:23:09 -0700

    build latest parse cardButton

--- build/news-grid/frontend.asset.php
+++ build/news-grid/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'c54c80fa1b9e8d79e18f');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'b7533d36c4d51e5a3396');
