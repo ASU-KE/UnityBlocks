7376ffbcf5600777715a5af6f20821f7ac805c14
John Cox <john.k.cox@asu.edu>
2025-06-20 11:50:04 -0700

    build latest cardButton props

--- build/news-grid/index.asset.php
+++ build/news-grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5e7800f85394ded1e4c9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd26ecc24832ab2b59342');
