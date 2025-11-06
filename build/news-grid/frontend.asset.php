1a31ae9fd572314f1a28ad12ca5422247cdaf711
John Cox <john.k.cox@asu.edu>
2025-11-06 15:45:46 -0700

    build latest simplify logic

--- build/news-grid/frontend.asset.php
+++ build/news-grid/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-element'), 'version' => 'c82e5c127d447147a0ac');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-element'), 'version' => '6b113b508d1fab063b56');
--- build/news-grid/index.asset.php
+++ build/news-grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7cb41b1bdf6074b8eeef');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0bbacf67276333e720a6');
