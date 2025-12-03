32c86d6c121ef250e4cfbf58038795ae81673d37
John Cox <john.k.cox@asu.edu>
2025-12-03 14:51:29 -0700

    build latest v3.0.1 component

--- build/asu-events/index.asset.php
+++ build/asu-events/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '89f207e96534370bc2d0');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bf0b8054fbc9bd40e5b6');
