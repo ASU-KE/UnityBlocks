efc5e674ae4077f3b1c5cf58a385fa8d6f37556d
John Cox <john.k.cox@asu.edu>
2025-12-09 16:44:19 -0700

    build v2.1 - news views update

--- build/news-grid/frontend.asset.php
+++ build/news-grid/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-element'), 'version' => '6b113b508d1fab063b56');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-element'), 'version' => '5b00113d010a07bafdf2');
--- build/news-grid/index.asset.php
+++ build/news-grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0bbacf67276333e720a6');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1aa0585cb478915e413e');
