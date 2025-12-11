f3f47c852fa5de940a5c6dbc70f00266f34bb572
John Cox <john.k.cox@asu.edu>
2025-12-11 09:52:49 -0700

    build dpercated_v1

--- build/news-grid/frontend.asset.php
+++ build/news-grid/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-element'), 'version' => '6b113b508d1fab063b56');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-element'), 'version' => '0ef7465daf2d20aec4f4');
