4cdf73cccd5f5d0f32178dbd46a7da6286022a2f
John Cox <john.k.cox@asu.edu>
2025-12-11 10:40:00 -0700

    build latest - depercated btn text

--- build/news-grid/frontend.asset.php
+++ build/news-grid/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-element'), 'version' => '0ef7465daf2d20aec4f4');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-element'), 'version' => 'e2600206eff53bb7740e');
