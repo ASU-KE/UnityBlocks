f4e438d49dca64d1b543f2b5eebe94ba80ac7e33
John Cox <john.k.cox@asu.edu>
2025-12-29 13:49:29 -0700

    v3 - update requirements, register via block manifest

--- unityblocks.php
+++ unityblocks.php
@@ -3,9 +3,9 @@
 /**
  * Plugin Name:       UnityBlocks
  * Description:       UnityBlocks is a suite of page building content blocks for the ASU Web Standards Unity (UDS) WordPress theme.
- * Requires at least: 6.1
- * Requires PHP:      7.0
- * Version:           2.5.1
+ * Requires at least: 6.8
+ * Requires PHP:      7.4
+ * Version:           3.0.0
  * Author:            ASU KE Web Services
  * Author URI:        https://rto.asu.edu/web-services
  * License:           GPL-2.0-or-later
@@ -35,23 +35,20 @@ function unityblocks_block_init()
 	/**
 	 * Load current theme and check if it is Pitchfork or a Pitchfork Child
 	 */
-	$theme_data = wp_get_theme();
-	$pitchfork_theme = ('pitchfork' === $theme_data->get('TextDomain') || 'pitchfork' === $theme_data->get('Template'));
+	//$theme_data = wp_get_theme();
+	//$pitchfork_theme = ('pitchfork' === $theme_data->get('TextDomain') || 'pitchfork' === $theme_data->get('Template'));
 
 	// Register these blocks only if not using Pitchfork
 	// Pitchfork already has these blocks
-	if (!$pitchfork_theme) {
-		register_block_type(__DIR__ . '/build/hero');
-	}
+	// if (!$pitchfork_theme) {
+	// 	register_block_type(__DIR__ . '/build/hero');
+	// } find new solution that works with block manifest
 
-	register_block_type(__DIR__ . '/build/anchor-menu');
-	register_block_type(__DIR__ . '/build/asu-careers');
-	register_block_type(__DIR__ . '/build/asu-events');
-	register_block_type(__DIR__ . '/build/events-grid');
-	register_block_type(__DIR__ . '/build/image-gallery');
-	register_block_type(__DIR__ . '/build/news-grid');
-	register_block_type(__DIR__ . '/build/testimonial');
-	register_block_type(__DIR__ . '/build/wchm');
+	wp_register_block_types_from_metadata_collection(
+    plugin_dir_path( __FILE__ ) . 'build',
+    plugin_dir_path( __FILE__ ) . 'build/blocks-manifest.php'
+);
+
 }
 add_action('init', 'unityblocks_block_init');
 
