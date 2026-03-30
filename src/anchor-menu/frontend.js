7cbae19a9d13bf4a57ded63d7a989318d233a4bc
John Cox <john.k.cox@asu.edu>
2026-03-30 15:16:17 -0700

    rm unused file

--- src/anchor-menu/frontend.js
+++
@@ -1,3 +0,0 @@
-// The anchor menu is automatically initialized by the Unity Bootstrap Theme JS
-// (loaded via the unityblocks-anchor-menu-view-script handle) when an element
-// with id="uds-anchor-menu" is present in the DOM.
