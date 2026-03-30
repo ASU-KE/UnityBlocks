32ca4d7e9434f965ed1b7be4914bd9218ce0a5a0
John Cox <john.k.cox@asu.edu>
2026-03-30 15:16:58 -0700

    rm get anchor custom script

--- src/anchor-menu/get-anchors.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-    const classPrefix = 'anchor-item-';
-    const anchors = document.querySelectorAll(`[class*="${classPrefix}"]`);
-    anchors.forEach(anchor => {
-        const anchorClass = anchor.className.split(' ').find(cls => cls.startsWith(classPrefix));
-        anchor.id = anchorClass;
-    });
-});
