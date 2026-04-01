ace560c96befefafbed2f1d3be7149b68c12bda0
John Cox <john.k.cox@asu.edu>
2026-04-01 14:38:32 -0700

    build latest - data-ga section update

--- build/anchor-menu/index.asset.php
+++ build/anchor-menu/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '24e55acfdf70b8c6246c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4591e447e8128374e237');
