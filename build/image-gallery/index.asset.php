e6001c53836bb5a5317511a17b1d3532190488b4
John Cox <john.k.cox@asu.edu>
2026-04-29 14:38:00 -0700

    build latest block api v3

--- build/image-gallery/index.asset.php
+++ build/image-gallery/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8e881a9d4056951fc0b0');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '50d87798fa8e0314446b');
